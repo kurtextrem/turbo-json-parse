7e8db8ae59257d841729b71d8012a0be72df664e
Jacob Groß <kurtextrem@gmail.com>
2021-10-07 16:09:20 +0200

    Update types.d.ts

--- types.d.ts
+++ types.d.ts
@@ -1,17 +1,20 @@
 interface ITurboJsonConfig {
-  buffer: boolean;
-  required:boolean;
-  ordered: boolean;
-  validate: boolean;
-  validateStrings: boolean;
-  fullMatch: boolean;
-  unescapeStrings: boolean;
-  defaults: boolean;
-  prettyPrinted: boolean;
+    buffer: boolean;
+    required: boolean;
+    ordered: boolean;
+    validate: boolean;
+    validateStrings: boolean;
+    fullMatch: boolean;
+    unescapeStrings: boolean;
+    defaults: boolean;
+    prettyPrinted: boolean;
 }
 
-declare function compile (input: string): object;
+declare function compile(input: string | Buffer): Record<string, unknown>;
 
-declare function turboJsonParse (schema: object, config?: ITurboJsonConfig): typeof compile;
+declare function turboJsonParse(
+    schema: Record<string, unknown>,
+    config?: ITurboJsonConfig,
+): typeof compile;
 
 export = turboJsonParse
